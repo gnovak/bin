15cea01e64496c3f8ee8c01b95e4cb4304c8a9bb
novak <>
2006-03-10 21:44:40 +0000

    [project @ 2006-03-10 21:44:39 by novak]

--- gas.pm
+++ gas.pm
@@ -1,9 +1,9 @@
 package gas;
 
 # initialize package globals, first exported ones
-#$dayofweek = "Tuesday";
+#$dayofweek = "Thursday";
 #$room = "ISB 356";
-#$time = "4-5pm";
+#$time = "12:30-1:30pm";
 #$sep = "#";
 #$infoFile = "gas.talks";
 
